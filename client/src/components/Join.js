25c5fb2a4b0c6519684e86b10bee298ada9fe6b6
chiaraintech <chiaraintech@gmail.com>
2021-04-21 15:26:42 +0100

    Join page starting to take form.

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,27 @@
-import React from 'react';
-                           
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+        
+//When the user joins, a connection event will be fired up.
+//When the user leaves, a disconnection event will be fired up instead.
 
 const Join = () => {
-    return <h1>Join</h1>
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return (
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading"></h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)}/></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e) => setRoom(e.target.value)}/></div>
+                <Link>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+                <div></div>
+            </div>
+        </div>
+    )
 }
 
 export default Join;
